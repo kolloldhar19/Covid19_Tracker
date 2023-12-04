12490482af6783f7aa1082f72a47885a71bff89e
kolloldhar19 <kollol.dhar19jan@gmail.com>
2023-12-04 17:30:40 +0530

    Adding Files

---
+++ src/test/java/com/covid_tracker/Covid19_Tracker/Covid19TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid_tracker.Covid19_Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
